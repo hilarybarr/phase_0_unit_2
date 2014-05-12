c57778a6b8f7ab1882c7cb64d0ca66304fca8fcb
hilarybarr <hilaryrosebarr@gmail.com>
2014-05-12 14:10:52 -0400

    Week 4 Challenge 3 Person 3

--- week_4/3_group_research_methods/my_solution.rb
+++ week_4/3_group_research_methods/my_solution.rb
@@ -1,9 +1,9 @@
 # U2.W4: Research Methods
 
-i_want_pets = ["I", "want", 3, "pets", "but", "I", "only", "have", 2]
-my_family_pets_ages = {"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, 
-            "Annabelle" => 0, "Ditto" => 3}
 
+
+
+=begin
 # Person 1's solution
 def my_array_finding_method(source, thing_to_find)
   # Your code here!
@@ -32,21 +32,58 @@ end
 #
 #
 
+=end
+
+
+
+
+i_want_pets = ["I", "want", 4, "pets", "but", "I", "only", "have", 3]
+my_family_pets_ages={"Evi" => 8, "Hoobie" => 5, "George" => 14, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}
+
 
 # Person 3
+
+# Create a method that alphabetizes an array of integers and strings, with integers appearing first, followed by capital and then lowercase
+# words. Each element will only appear once and each element will be in string form in alphabetized array.
 def my_array_sorting_method(source)
-  # Your code here!
+  # Removes duplicates and converts every integer to a string.
+  # Create new array  as opposed to use source! because don't want to modify existing data structure
+  source_2=source.uniq.map!{|x| x.to_s}
+  # Sorts array in alphabetical order 
+  source_2.sort{|a,b| a <=> b}
 end
 
+print my_array_sorting_method(i_want_pets) 
+
+puts my_array_sorting_method(i_want_pets) == ["3", "4", "I", "but", "have", "only", "pets", "want"]
+
+
+
+# Create a method that sorts a hash by age.
 def my_hash_sorting_method(source)
-  # Your code here!
+ source.sort_by{|name, age| age}
 end
+
+
+print my_hash_sorting_method(my_family_pets_ages)
+
+# This may be false depending on how your method deals with ordering the animals with the same ages.
+puts my_hash_sorting_method(my_family_pets_ages) == [["Annabelle", 2], ["Ditto", 5], ["Hoobie", 5], ["Bogart", 6], ["Poly", 6], ["Evi", 8], ["George", 14]] 
+
+
+
 
 # Identify and describe the ruby method you implemented. 
-# 
-#
-#
+# array.uniq: removes duplicate elements
+# array.map: returns a new array with the results of running the block once for every element in enum. 
+#            (Similar to .each, except each returns original object)
+# .to_s: converts integers to strings
+# array.sort {|a,b| a<=>b}: sorts in alphabetical order
+# hash.sort_by {|a,b| b}: sorts by value in ascending order
 
+
+
+=begin
 
 # Person 4
 def my_array_deletion_method(source, thing_to_delete)
@@ -62,6 +99,7 @@ end
 #
 #
 
+=end
 
 ################## DRIVER CODE ###################################
 # HINT: Use `puts` statements to see if you are altering the original structure with these methods. 
@@ -79,7 +117,9 @@ p my_hash_deletion_method(my_family_pets_ages, "George") == {"Evi" => 8, "Hoobie
 
 # Reflect!
 # 
+# Learned difference .collect (a method of enumerable) and .length
 # 
 # 
 # 
-# 
+
+=end
