0e6d8a357c54205fe9ac8375b8bff7ae2d8be3bc
hilarybarr <hilaryrosebarr@gmail.com>
2014-05-29 21:39:02 -0400

    Week 6

--- week_6/5_refactoring/my_solution.rb
+++ week_6/5_refactoring/my_solution.rb
@@ -5,6 +5,15 @@
 
 
 # Original Solution
+    else 
+      raise ArgumentError.new("Must enter 16 numbers")
+    end
+  end
+  
+  def even_total
+      even_index_array = @card_number.select.each_with_index { |num, i| i.even? }
+      even_times_two = even_index_array.map {|x| x.to_i*2}
+      even_times_two = even_times_two.map! do |x| 
 
 
 
